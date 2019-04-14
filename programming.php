23ce92302141103c7ee98f1d53c4a074cf7fa8ba
konradszwed <konradszwed11@gmail.com>
2019-04-14 15:37:58 +0200

    programowanie web

--- programming.php
+++ programming.php
@@ -1,18 +1,46 @@
-<script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
 <div class="outer-container portfolio-page">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-12 col-md-12 col-lg-12 ">
-                <h3><a href="https://github.com/konradszwed">Aktualnie najważniejsze dla mnie projekty znajdziecie na moim repozytorium <i class="fa fa-github" aria-hidden="true"></i></a></h3>
+            
+            <div class="col-12 col-md-12 col-lg-12 ">
+                <div class="portfolio-content entry-content flex flex-column  justify-content-center" style="visibility: visible; opacity: 1;">
+                    <?php #echo file_get_contents('media/private/.txt'); ?>
+                    
+                    <h1>Tworzenie serwisów internetowych</h1>
+                    <br/>
+                    <h4><a href="https://github.com/konradszwed">Aktualnie rozwijane projekty znajdziecie na moim GitHubie <i class="fa fa-github" aria-hidden="true"></i></a></h4>
+                    <br/>
+                    Nie zamierzam zaśmiecać serwerów jakimiś prostymi programami z uczelni, toteż postaram się systematycznie wstawiać i opisywać te co ciekawsze. 
+
+                    <br/>
+                    <br/>
+                    Projekty:<br/>
+                    <ol>
+                      <li><a href="https://github.com/konradszwed/projekt_analiza_obrazu">Analiza grafiki na serwisach internetowych</a>
+                          <br/>
+                          Python (numpy, imageio, matplotlib, csv), PHP, inne technologie webowe które pozwolą mi całość przenieść i działać na serwerze.
+                          <br/>
+                          Aktualnie wciąż tylko algorytm pobierający z konkretnego miejsca podstawowe dane o plikach graficznych weń występujących mający na celu podać uśrednione wyniki i wykresy. W przyszłości ma to być aplikacja webowa głównie oparta na technologiach Python i PHP pokazująca analizę widmową, technikalia podanej grupy witryn. Pozwoliłoby to na analizę zmian w trendach grafiki na serwisach o konkretnych kategoriach. W ostatnim etapie chciałbym dodać moduł Machine Learning potrafiący przewidywać nadchodzące trendy.
+                              
+                    </li>
+                        
+                    <li><a href="https://github.com/konradszwed/Mail_bot">Mail Bot </a>
+                        <br/>
+                        Python. Bot do rozsyłania maili.
+                    </li>
+                        
+                    <li><a href="https://github.com/konradszwed">Pozostałe projekty postaram się opisać w kolejnej wolnej chwili </a>
+                        <br/>
+                        Bez odbioru.
+                    </li>
+
+                    </ol>
+                </div>
             </div><!-- .col -->
+            
+                 
+            
         </div><!-- .row -->
-        
-        <div class="row">
-            <div class="hidden-xs col-sm-12 col-md-3 col-lg-3 ">
-                <a href="https://github.com/konradszwed"><i class="fa fa-github"></i></a>
-            </div><!-- .col -->
-        </div><!-- .row -->
-        
     </div><!-- .container-fluid -->
 
 </div><!-- .outer-container -->
